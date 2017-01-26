416e46c40a0b080cf1516866c7e460bfe9ae547e
DanielAirgood <da8@students.ptcollege.edu>
2017-01-26 16:23:21 -0500

    Add files via upload

---
+++ ExperimentalProc/ExperimentalProc/Data.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentalProc
+{
+    class Data
+    {
+        //anything to be inherited to all data is to go in this class
+        protected Data()//Data class constructor
+        {
+
+        }
+    }
+
+    class Student : Data
+    {
+        //anything to be inherited by all students go in this class
+
+        private string StudentName = null;
+
+        protected Student()//Student class constructor
+        {
+
+        }
+
+        public void setStudentName(string StudentName)
+        {
+            this.StudentName = StudentName;
+        }
+
+        public string getStudentName()
+        {
+            return StudentName;
+        }
+    }
+}
