c9d4ff1f135d9e3c593a8cd783a862c026cbe609
DanielAirgood <da8@students.ptcollege.edu>
2017-03-08 16:28:50 -0500

    Add files via upload

---
+++ SchedularLibrary/SchedularLibrary/ClientManager/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentalProc.ClientManager
+{
+    public class Client
+    {
+        protected int SessionID;
+
+        public Client(int SessionID)
+        {
+
+
+        }
+
+        public int getSessionID()
+        {
+            return SessionID;
+        }
+    }
+}
---
+++ SchedularLibrary/SchedularLibrary/ClientManager/Manager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentalProc.ClientManager
+{
+    public class Manager
+    {
+        List<Client> ActiveUsers;
+
+        public Manager()
+        {
+            ActiveUsers = new List<Client>();
+        }
+
+        public Client RetriveClient(int ClientID)
+        {
+            return ActiveUsers.Find(x => x.getSessionID() == ClientID);//TODO: alter this to use index
+        }
+    }
+}
